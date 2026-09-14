b8e17eab2a514876f33c17e1eecc412bcf2fe0ae
Leah Itagaki <litagaki@nvidia.com>
2026-09-14 12:33:38 -0400

    fix(rest-api): serialize empty DPU config collections
    Normalize DPU network configuration collection fields so empty Core protobuf values are returned as JSON arrays instead of null. Preserve populated values and cover empty and populated cases with named table-driven subtests.
    
    Signed-off-by: Leah Itagaki <litagaki@nvidia.com>

--- rest-api/api/pkg/api/model/dpumachine.go
+++ rest-api/api/pkg/api/model/dpumachine.go
@@ -116,7 +116,7 @@ func (apnnc *APIDpuNetworkConfig) FromProto(protoConfig *corev1.ManagedHostNetwo
 	}
 
 	apnnc.Asn = protoConfig.Asn
-	apnnc.DhcpServers = protoConfig.DhcpServers
+	apnnc.DhcpServers = append(make([]string, 0, len(protoConfig.DhcpServers)), protoConfig.DhcpServers...)
 	apnnc.VniDevice = protoConfig.VniDevice
 	apnnc.ManagedHostConfigVersion = protoConfig.ManagedHostConfigVersion
 	apnnc.UseAdminNetwork = protoConfig.UseAdminNetwork
@@ -131,17 +131,17 @@ func (apnnc *APIDpuNetworkConfig) FromProto(protoConfig *corev1.ManagedHostNetwo
 		nvt := protoConfig.GetNetworkVirtualizationType().String()
 		apnnc.NetworkVirtualizationType = &nvt
 	}
-	apnnc.RouteServers = protoConfig.RouteServers
+	apnnc.RouteServers = append(make([]string, 0, len(protoConfig.RouteServers)), protoConfig.RouteServers...)
 	apnnc.RemoteID = protoConfig.RemoteId
-	apnnc.DeprecatedDenyPrefixes = protoConfig.DeprecatedDenyPrefixes
-	apnnc.DenyPrefixes = protoConfig.DenyPrefixes
-	apnnc.SiteFabricPrefixes = protoConfig.SiteFabricPrefixes
+	apnnc.DeprecatedDenyPrefixes = append(make([]string, 0, len(protoConfig.DeprecatedDenyPrefixes)), protoConfig.DeprecatedDenyPrefixes...)
+	apnnc.DenyPrefixes = append(make([]string, 0, len(protoConfig.DenyPrefixes)), protoConfig.DenyPrefixes...)
+	apnnc.SiteFabricPrefixes = append(make([]string, 0, len(protoConfig.SiteFabricPrefixes)), protoConfig.SiteFabricPrefixes...)
 	apnnc.VpcIsolationBehavior = protoConfig.VpcIsolationBehavior.String()
 	apnnc.StatefulAclsEnabled = protoConfig.StatefulAclsEnabled
 	apnnc.EnableDhcp = protoConfig.EnableDhcp
 	apnnc.IsPrimaryDpu = protoConfig.IsPrimaryDpu
 	apnnc.DatacenterAsn = protoConfig.DatacenterAsn
-	apnnc.AnycastSitePrefixes = protoConfig.AnycastSitePrefixes
+	apnnc.AnycastSitePrefixes = append(make([]string, 0, len(protoConfig.AnycastSitePrefixes)), protoConfig.AnycastSitePrefixes...)
 	apnnc.TenantHostAsn = protoConfig.TenantHostAsn
 	apnnc.SiteGlobalVpcVni = protoConfig.SiteGlobalVpcVni
 
@@ -155,13 +155,11 @@ func (apnnc *APIDpuNetworkConfig) FromProto(protoConfig *corev1.ManagedHostNetwo
 		apnnc.AdminInterface.FromProto(protoConfig.AdminInterface)
 	}
 
-	if protoConfig.TenantInterfaces != nil {
-		apnnc.TenantInterfaces = make([]APIFlatInterfaceConfig, len(protoConfig.TenantInterfaces))
-		for i, protoInterface := range protoConfig.TenantInterfaces {
-			if protoInterface != nil {
-				apnnc.TenantInterfaces[i] = APIFlatInterfaceConfig{}
-				apnnc.TenantInterfaces[i].FromProto(protoInterface)
-			}
+	apnnc.TenantInterfaces = make([]APIFlatInterfaceConfig, len(protoConfig.TenantInterfaces))
+	for i, protoInterface := range protoConfig.TenantInterfaces {
+		if protoInterface != nil {
+			apnnc.TenantInterfaces[i] = APIFlatInterfaceConfig{}
+			apnnc.TenantInterfaces[i].FromProto(protoInterface)
 		}
 	}
 
